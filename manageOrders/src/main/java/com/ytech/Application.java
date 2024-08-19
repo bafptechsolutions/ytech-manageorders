d535ac8a49a2becdd3cf5b8fed5c11fd11b613f6
Bruno Pinto <bafpinto@gmail.com>
2024-08-19 14:20:06 +0100

    working inicial code

---
+++ manageOrders/src/main/java/com/ytech/Application.java
@@ -0,0 +1,16 @@
+package com.ytech;
+
+/**
+ * @author Bruno Pinto
+ * @since 18/08/2024
+ */
+public class Application {
+  public static void main(String[] args) {
+
+    // teste
+    System.out.println("Test - Hello and welcome!");
+    for (int i = 1; i <= 5; i++) {
+      System.out.println("i = " + i);
+    }
+  }
+}
